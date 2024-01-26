33163149dcd65659b86be08ad3d0b69d10787c6e
jo.seongjin <jo.seongjin@mkinternet.com>
2024-01-26 14:12:58 +0900

    refactor : BaseRepository update, delete 구현 메서드 생성

--- lib/class/repository/BaseRepository.php
+++ lib/class/repository/BaseRepository.php
@@ -33,7 +33,7 @@ abstract class BaseRepository{
         }
     }
 
-    protected function update($table, $data, $whereArr){
+    protected function update($table, $data, $where_data){
         try {
             // SET 부분 변수
             $set = implode(', ', array_map(function ($column) {
@@ -42,24 +42,36 @@ abstract class BaseRepository{
 
             // WHERE 부분 변수
             $where = '';
-            if (!empty($whereArr)) {
+            if (!empty($where_data)) {
                 $where = 'WHERE ' . implode(' AND ', array_map(function ($column) {
                         return "$column = :$column";
-                    }, array_keys($whereArr)));
+                    }, array_keys($where_data)));
             }
 
             $query = "UPDATE $table SET {$set} {$where}";
-            $params = array_merge($data, $whereArr);
+            $params = array_merge($data, $where_data);
             $stmt = $this->pdo->prepare($query);
             $stmt->execute($params);
         } catch (\PDOException $e) {
-            // 오류 메시지를 기록하거나 출력합니다.
             echo '오류: ' . $e->getMessage();
         }
     }
 
-    protected function delete($table, $where){
+    protected function delete($table, $where_data) {
+        try {
+            $where = '';
+            if (!empty($where_data)) {
+                $where = 'WHERE ' . implode(' AND ', array_map(function ($column) {
+                        return "$column = :$column";
+                    }, array_keys($where_data)));
+            }
 
+            $query = "DELETE FROM {$table} {$where}";
+            $stmt = $this->pdo->prepare($query);
+            $stmt->execute($where_data);
+        } catch (\PDOException $e) {
+            echo '오류: ' . $e->getMessage();
+        }
     }
 
     // select 하는게 sql마다 다를 수 있음 ,,,,,
