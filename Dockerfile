91d1b1c43940457b94a73fcb1c692c490df7ec5e
seongjin8860 <seongjin8860@naver.com>
2024-11-30 23:13:52 +0900

    feat : composer 활용 autoload 적용

--- Dockerfile
+++ Dockerfile
@@ -4,15 +4,25 @@ FROM php:8.2-apache
 # 필요한 php 확장 설치
 RUN apt-get update && docker-php-ext-install pdo pdo_mysql
 
+# Composer 설치
+RUN curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
+
+# Xdebug 설치
+RUN pecl install xdebug && docker-php-ext-enable xdebug
+
 # 기본 디렉토리 경로 설정
 WORKDIR /var/www/html/
 
 # 로컬 php 파일 컨테이너로 복사
 COPY . .
 
+# 오토로더 갱신
+RUN composer dump-autoload --optimize
+
 # 웹 서버가 파일을 읽을 수 있게.
 RUN chown -R www-data:www-data /var/www/html && chmod -R 777 /var/www/html
 
 # 아파치 기본 포트 설정
 EXPOSE 80
+
 # CMD ["apache2-foreground"]
--- lib/config.php
+++ lib/config.php
@@ -2,6 +2,8 @@
 //error_reporting(E_ALL);
 //ini_set('display_errors', 1);
 
+require '/var/www/html/vendor/autoload.php';  // 절대 경로로 수정
+
 // 경로 변수 설정
 define('DOCUMENT_ROOT', "/var/www/html");
 define('LIB_PATH', DOCUMENT_ROOT . "/lib");
@@ -12,21 +14,6 @@ define('FILE_PATH', DOCUMENT_ROOT . "/file");
 define('INCULEDS_PATH',DOCUMENT_ROOT . "/includes");
 define('PAGES_PATH',DOCUMENT_ROOT . '/pages');
 define('ADMIN_PATH',PAGES_PATH . '/admin');
-
-
-//오토로드
-// /var/www/html/autoload.php
-function myAutoloader($className) {
-    $className = str_replace('\\', '/', $className);
-    $path = CLASS_PATH . DIRECTORY_SEPARATOR . "{$className}.php";
-//    echo $path;
-    if (file_exists($path)) {
-        require_once $path;
-        return;
-    }
-}
-// 오토로더 함수를 등록
-spl_autoload_register('myAutoloader');
 
 
 
